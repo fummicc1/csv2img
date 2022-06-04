5d75ed8c68f769b5485043ce4eb90de470765730
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-04 15:34:55 +0900

    Test

--- Package.swift
+++ Package.swift
@@ -7,6 +7,12 @@ let package = Package(
     name: "Csv2Img",
     platforms: [.macOS(.v10_14)],
     products: [
+        .library(
+            name: "Csv2Img_Csv2ImgCmd",
+            targets: [
+                "Csv2Img", "Csv2ImgCmd"
+            ]
+        ),
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Csv2Img",
@@ -24,7 +30,11 @@ let package = Package(
             name: "ArgumentParser",
             url: "https://github.com/apple/swift-argument-parser",
             from: .init(1, 1, 2)
-        )
+        ),
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin.git",
+            from: "1.0.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -37,10 +47,7 @@ let package = Package(
             dependencies: ["Csv2Img"]),
         .executableTarget(
             name: "Csv2ImgCmd",
-            dependencies: ["Csv2Img", "ArgumentParser"],
-            resources: [
-                .copy("Resources")
-            ]
+            dependencies: ["Csv2Img", "ArgumentParser"]            
         )
     ]
 )
