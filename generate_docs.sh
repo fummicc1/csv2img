5e8b0c7ed189a7036b1ab6639a40fbc1e6b344f1
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-07 15:24:37 +0900

    Update documentation

---
+++ generate_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+jazzy \
+  --clean \
+  --author fummicc1 \
+  --author_url https://fummicc1.dev \
+  --module Csv2Img \
+  --output docs/Csv2Img
+
+jazzy \
+  --clean \
+  --author fummicc1 \
+  --author_url https://fummicc1.dev \
+  --module Csv2ImgCmd \
+  --output docs/Csv2ImgCmd
