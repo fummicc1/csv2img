a84e6cbefb064990080573593dac724c8f685af5
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-12 21:08:57 +0900

    Reset changes

--- Csv2ImageApp/Csv2ImageApp/ContentView.swift
+++ Csv2ImageApp/Csv2ImageApp/ContentView.swift
@@ -8,30 +8,46 @@
 import SwiftUI
 import Csv2Img
 
+
 enum CsvImageAppError: Swift.Error {
     case invalidNetworkURL(url: String)
+    case outputFileNameIsEmpty
+    case underlying(Error)
+
+    var message: String {
+        switch self {
+        case .invalidNetworkURL(let url):
+            return "Invalid URL: \(url)"
+        case .outputFileNameIsEmpty:
+            return "Empty Output FileName"
+        case .underlying(let error):
+            return "\(error)"
+        }
+    }
 }
 
 struct ContentView: View {
 
-    @State private var error: Error?
+    @State private var error: CsvImageAppError?
     @State private var networkURL: String = ""
     @State private var showNetworkFileImporter: Bool = false
     @State private var showFileImporter: Bool = false
     @State private var csv: Csv?
+    @State private var completeSavingFile: Bool = false
+    @State private var savedOutputFileURL: URL?
 
-    #if os(iOS)
+#if os(iOS)
     @State private var outputFileName: String = ""
-    #endif
+#endif
 
     var body: some View {
         GeometryReader { proxy in
             VStack {
-                #if os(iOS)
+#if os(iOS)
                 TextField("Please Decide Output FileName", text: $outputFileName)
-                    .font(.title3)
+                    .font(.body)
                     .padding()
-                #endif
+#endif
                 if showNetworkFileImporter {
                     HStack {
                         TextField("Input URL", text: $networkURL)
@@ -41,7 +57,7 @@ struct ContentView: View {
                                     csv = try Csv.fromURL(url)
                                     showNetworkFileImporter = false
                                 } catch {
-                                    self.error = error
+                                    self.error = .underlying(error)
                                 }
                             } else {
                                 self.error = CsvImageAppError.invalidNetworkURL(url: networkURL)
@@ -52,72 +68,24 @@ struct ContentView: View {
                 }
                 if let csv = csv, let img = csv.cgImage(fontSize: 12) {
                     Text("Output Image")
-                        .font(.title)
+                        .font(.title3)
                         .bold()
                     ScrollView {
                         Image(img, scale: 1, orientation: .up, label: Text("Output Image"))
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: proxy.size.width * 0.6)
+                            .frame(maxWidth: proxy.size.width * 0.8)
                     }
                 }
+#if os(macOS)
                 HStack {
-                    Button {
-                        showFileImporter = true
-                    } label: {
-                        Text("Choose Csv File from Local Computer.")
-                            .font(.title3)
-                            .bold()
-                            .padding()
-                    }
-                    .padding()
-
-                    Button {
-                        showNetworkFileImporter = true
-                    } label: {
-                        Text("Choose Csv File from Network.")
-                            .font(.title3)
-                            .bold()
-                            .padding()
-                    }
-                    .padding()
-                    Spacer()
-                    Button {
-                        #if os(macOS)
-                        let panel = NSSavePanel()
-                        panel.allowedContentTypes = [.png]
-                        panel.begin { response in
-                            switch response {
-                            case .OK:
-                                if let url = panel.url {
-                                    csv?.write(to: url)
-                                }
-                            default:
-                                break
-                            }
-                        }
-                        #elseif os(iOS)
-                        guard let document = FileManager.default.urls(
-                            for: .documentDirectory,
-                            in: .userDomainMask
-                        ).first else {
-                            fatalError()
-                        }
-                        let url = document.appendingPathComponent(
-                            outputFileName,
-                            conformingTo: .png
-                        )
-                        let _ = csv?.write(to: url)
-                        #endif
-                    } label: {
-                        Text("Save Output Image.")
-                            .font(.title3)
-                            .bold()
-                            .padding()
-                    }
-                    .disabled(csv == nil)
-                    .padding()
+                    actions()
                 }
+#elseif os(iOS)
+                VStack(spacing: 0) {
+                    actions()
+                }
+#endif
             }
         }.fileImporter(
             isPresented: $showFileImporter,
@@ -129,13 +97,31 @@ struct ContentView: View {
                     self.csv = try Csv.fromFile(url)
                 } catch {
                     print(error.localizedDescription)
-                    self.error = error
+                    self.error = .underlying(error)
                 }
             case .failure(let error):
-                self.error = error
+                self.error = .underlying(error)
             }
         }
-        .alert("Error Occurred.",
+        .alert("Complete Saving file!", isPresented: $completeSavingFile) {
+            Button {
+                completeSavingFile = false
+            } label: {
+                Text("Close")
+            }
+            Button {
+                completeSavingFile = false
+                if let url = savedOutputFileURL {
+                    if url.startAccessingSecurityScopedResource(), Application.shared.canOpenURL(url) {
+                        Application.shared.open(url, options: [:])
+                    }
+                    url.stopAccessingSecurityScopedResource()
+                }
+            } label: {
+                Text("Open File")
+            }
+        }
+        .alert(error?.message ?? "",
                isPresented: Binding(
                 get: {
                     error != nil
@@ -146,9 +132,73 @@ struct ContentView: View {
             Button {
                 error = nil
             } label: {
-                Text(String(describing: error))
+                Text("Close")
             }
         }
+    }
+
+    @ViewBuilder
+    func actions() -> some View {
+        Button {
+            showFileImporter = true
+        } label: {
+            Text("Choose Csv File from Device.")
+                .font(.body)
+                .bold()
+        }
+        .buttonStyle(.bordered)
+        .padding()
+
+        Button {
+            showNetworkFileImporter = true
+        } label: {
+            Text("Choose Csv File from Network.")
+                .font(.body)
+                .bold()
+        }
+        .buttonStyle(.bordered)
+        .padding()
+        Spacer()
+        Button {
+#if os(macOS)
+            let panel = NSSavePanel()
+            panel.allowedContentTypes = [.png]
+            panel.begin { response in
+                switch response {
+                case .OK:
+                    if let url = panel.url {
+                        let ok = csv?.write(to: url)
+                        completeSavingFile = ok ?? false
+                    }
+                default:
+                    break
+                }
+            }
+#elseif os(iOS)
+            if outputFileName.isEmpty {
+                error = CsvImageAppError.outputFileNameIsEmpty
+                return
+            }            
+            let document = URL(fileURLWithPath: NSHomeDirectory())
+            let url = document.appendingPathComponent(
+                outputFileName,
+                conformingTo: .png
+            )
+            let ok = csv?.write(to: url)
+            completeSavingFile = ok ?? false
+            if completeSavingFile {
+                savedOutputFileURL = url
+            }
+#endif
+        } label: {
+            Text("Save Output Image.")
+                .font(.body)
+                .bold()
+        }
+        .buttonStyle(.borderedProminent)
+        .disabled(csv == nil)
+        .padding()
+
     }
 }
 
