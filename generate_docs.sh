7e30628fa8d49394fd4db8730bec09c49274cc0f
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-07 22:57:07 +0900

    [hotfix] Updated docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-swift package --allow-writing-to-directory docs/Csv2Img \
+swift package --allow-writing-to-directory docs/ \
     generate-documentation --target Csv2Img \
     --disable-indexing \
     --transform-for-static-hosting \
-    --hosting-base-path csv2img/Csv2Img \
-    --output-path docs/Csv2Img
+    --hosting-base-path csv2img \
+    --output-path docs/
 
-swift package --allow-writing-to-directory docs/Csv2ImgCmd \
-    generate-documentation --target Csv2ImgCmd \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path csv2img/Csv2ImgCmd \
-    --output-path docs/Csv2ImgCmd
+cp res/app_privacy_policy.html docs/
