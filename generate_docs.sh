41aded419f06cb625f91b98f8fb8a23d4dc5c049
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-07 23:15:39 +0900

    [hotfix] Fixed docc

--- generate_docs.sh
+++ generate_docs.sh
@@ -4,14 +4,14 @@ swift package --allow-writing-to-directory docs/Csv2Img \
     generate-documentation --target Csv2Img \
     --disable-indexing \
     --transform-for-static-hosting \
-    --hosting-base-path csv2img \
+    --hosting-base-path csv2img/Csv2Img \
     --output-path docs/Csv2Img
 
 swift package --allow-writing-to-directory docs/Csv2ImgCmd \
     generate-documentation --target Csv2ImgCmd \
     --disable-indexing \
     --transform-for-static-hosting \
-    --hosting-base-path csv2img \
+    --hosting-base-path csv2img/Csv2ImgCmd \
     --output-path docs/Csv2ImgCmd
 
 
