1b0f3837be282dad4889ff8906fe70c9a5c9322b
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-07 22:44:21 +0900

    [hotfix] Fixed DocC generating command.

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-swift package generate-documentation
+swift package --allow-writing-to-directory docs/Csv2Img \
+    generate-documentation --target Csv2Img \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --hosting-base-path csv2img/Csv2Img \
+    --output-path docs/Csv2Img
 
+swift package --allow-writing-to-directory docs/Csv2ImgCmd \
+    generate-documentation --target Csv2ImgCmd \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --hosting-base-path csv2img/Csv2ImgCmd \
+    --output-path docs/Csv2ImgCmd
