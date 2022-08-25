544b99eb0dfa1a4b56e16fa5f52ad9b8749f25cf
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-25 17:15:39 +0900

    #8 Updated Examples

--- Examples/CsvBuilderExample/CsvBuilderExample/ContentView.swift
+++ Examples/CsvBuilderExample/CsvBuilderExample/ContentView.swift
@@ -9,17 +9,29 @@ import SwiftUI
 import CsvBuilder
 
 struct ContentView: View {
+
+    @State private var composition: ExampleComposition = .init()
+    @State private var image: CGImage?
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            if let image = image {
+                Image(nsImage: NSImage(cgImage: image, size: CGSize(width: image.width, height: image.height)))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
         }
         .padding()
-        .onAppear {
-            let example = ExampleComposition()
-            try! CsvBuilder.inject(composition: example)
+        .task {
+            composition.ages.append("99")
+            composition.names.append("Yamada")
+            let csv = try! CsvBuilder.build(composition: composition)
+            let data = try! await csv.generate(fontSize: 20, exportType: .png)
+            self.image = data.base as! CGImage
         }
     }
 }
