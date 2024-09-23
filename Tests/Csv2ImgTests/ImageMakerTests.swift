6359b04a6698b90ad2c820a4bf3f8f930cff4518
fummicc1 <fumiya.tennis1@gmail.com>
2024-09-23 19:43:34 +0900

    test: Add ImageMaker tests

---
+++ Tests/Csv2ImgTests/ImageMakerTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+@testable import Csv2Img
+
+class ImageMakerTests: XCTestCase {
+    func testMakeImage() async throws {
+        // Given
+        let csv = Csv.loadFromString(
+            """
+            name,beginnerValue,middleValue,expertValue,unit
+            Requirements Analysis,1.00,1.00,1.00,H
+            Concept Design,0.10,0.50,1.00,H
+            Detail Design,0.10,0.50,1.00,page
+            """,
+            styles: [
+                Csv.Column.Style(color: Color.blue.cgColor),
+                Csv.Column.Style(color: Color.blue.cgColor),
+                Csv.Column.Style(color: Color.blue.cgColor),
+                Csv.Column.Style(color: Color.blue.cgColor),
+                Csv.Column.Style(color: Color.blue.cgColor)
+            ]
+        )
+        let imageMaker = ImageMaker(maximumRowCount: nil, fontSize: 12)
+        // When
+        let image = try imageMaker.make(
+            columns: await csv.columns,
+            rows: await csv.rows
+        ) { double in
+        }
+        // Then
+        let packageRootPath = URL(fileURLWithPath: #file).pathComponents
+            .prefix(while: { $0 != "Tests" }).joined(
+                separator: "/"
+            ).dropFirst()
+        let fileURLPath = String(packageRootPath + "/Fixtures/outputs/category.png")
+        let fileURL = URL(fileURLWithPath: fileURLPath)
+        XCTAssertTrue(FileManager.default.fileExists(atPath: fileURL.path))
+        let expected = try Data(contentsOf: fileURL)
+        XCTAssertEqual(image.convertToData(), expected)
+    }
+}
