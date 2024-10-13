5053a173507bfca950c4fd694b4eae4d20f26fb1
fummicc1 <fumiya.tennis1@gmail.com>
2024-10-13 09:54:27 +0900

    chore: refactor scripts. Trying to support multi target docc

---
+++ scripts/generate_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+swift package --allow-writing-to-directory docs \
+    generate-documentation \
+    --target Csv2Img --target Csv2ImgCmd --target CsvBuilder \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --enable-experimental-combined-documentation \
+    --hosting-base-path csv2img \
+    --output-path docs
+
+cp res/app_privacy_policy.html docs/app_privacy_policy.html
+cp README.md docs/index.md
