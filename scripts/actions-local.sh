86dc9a3b672db50a0037dc2fdc3bd3799b1d8f1f
fummicc1 <fumiya.tennis1@gmail.com>
2024-10-17 00:40:46 +0900

    refactor

--- scripts/actions-local.sh
+++ scripts/actions-local.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -e
-set -x
 
 current_dir=`realpath $0`
 cd `dirname $current_dir`/../
 
 act -P macos-14=-self-hosted -l --container-architecture linux/amd64
 
+# Because xcode-setup does not work on local machine, make local flag true to skip xcode-setup
 export LOCAL=true
 
 act -P macos-14=-self-hosted -W ./.github/workflows/core.yml --container-architecture linux/amd64
